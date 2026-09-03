51ee011a6fa06be1b34ed93b6fd2a61f1ba957f9
riansabrido-cs <rian.sabrido@neu.edu.ph>
2026-09-03 21:59:08 +0800

    Create PowerOff.java

---
+++ commandPattern/PowerOff.java
@@ -0,0 +1,12 @@
+public class PowerOff implements Command {
+    private Tv tv;
+
+    public PowerOff(Tv tv) {
+        this.tv = tv;
+    }
+
+    @Override
+    public String execute() {
+        return tv.switchOff();
+    }
+}
