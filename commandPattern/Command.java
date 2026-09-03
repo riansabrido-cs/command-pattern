5be78f5cfa5c1f19bb0d391bbc6ca1d3c562dcec
riansabrido-cs <rian.sabrido@neu.edu.ph>
2026-09-03 21:58:24 +0800

    Create Command.java

---
+++ commandPattern/Command.java
@@ -0,0 +1,21 @@
+public interface Command {
+    public String execute();
+}
+```[cite: 1]
+
+**PowerOn.java**
+```java
+package commandPattern;
+
+public class PowerOn implements Command {
+    private Tv tv;
+
+    public PowerOn(Tv tv) {
+        this.tv = tv;
+    }
+
+    @Override
+    public String execute() {
+        return tv.switchOn();
+    }
+}
