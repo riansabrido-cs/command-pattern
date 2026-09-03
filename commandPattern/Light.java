b2920a9be05ea74810152b95bd6be19dd3235853
riansabrido-cs <rian.sabrido@neu.edu.ph>
2026-09-03 22:08:15 +0800

    Create Light.java

---
+++ commandPattern/Light.java
@@ -0,0 +1,9 @@
+public class Light {
+    public String turnOn() {
+        return "Light is turned on with default brightness!";
+    }
+
+    public String turnOff() {
+        return "Light is turned off!";
+    }
+}
