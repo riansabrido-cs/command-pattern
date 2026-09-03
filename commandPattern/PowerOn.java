fb9fcd4deb7629307d9893ca71b93ce5bae96a24
riansabrido-cs <rian.sabrido@neu.edu.ph>
2026-09-03 22:02:00 +0800

    Create PowerOn.java

---
+++ commandPattern/PowerOn.java
@@ -0,0 +1,12 @@
+public class PowerOn implements Command {
+    private Tv tv;
+
+    public PowerOn(Tv tv) {
+        this.tv = tv;
+    }
+
+    @Override
+    public String execute() {
+        return tv.switchOn();
+    }
+}
